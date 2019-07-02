8ae9d25754c599df6bc6812fa4219df6c2d341fb
Artur Tadrala <artur.tadrala@gmail.com>
2019-07-02 21:56:44 +0200

    Add StravaToCyclingMetricsConverter

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
+import { zip } from "lodash";
+
 export const Greeter = (name: string) => `Hello ${name}`;
+
+export const StravaToCyclingMetricsConverter = (secondsArr : Array<Number>, powerArr:Array<Number>, hrArray :Array<Number>) => {
+    return zip(secondsArr, powerArr, hrArray);
+}
--- test/test.ts
+++ test/test.ts
@@ -1,7 +1,36 @@
 import { suite, test, slow, timeout } from "mocha-typescript";
+import { assert } from "chai";
+import {StravaToCyclingMetricsConverter} from "../src/index";
+import { first } from 'lodash';
+import data from './sampleResponseStream.json';
 
 @suite(timeout(3000), slow(1000))
-class Hello {
-    @test world() {
+class StravaToCyclingMetricsConverterSuite {
+    
+    @test smokeTest() {
+        var seconds = [1,2,3,4];
+        var power = [110,120,130,140];
+        var hr = [90,92,93,94];
+
+        var results = StravaToCyclingMetricsConverter(seconds, power, hr);
+        assert.deepEqual(results, [[1,110,90], [2,120,92],[3,130,93], [4,140,94]], "result incorrect");
+    }
+
+    @test differentLengthShouldThrow() {
+        var seconds = [1,2];
+        var power = [110];
+        var hr = [90,92,93];
+        
+        assert.throws(() => StravaToCyclingMetricsConverter(seconds, power, hr));
+    }
+
+    @test assertSampleResponseStream() {
+        const dat: any[] = data;
+        
+        const time = dat.filter( x => x.type === 'time')[0];
+        const hr = dat.filter( x => x.type === 'heartrate')[0];
+        const power = dat.filter( x => x.type === 'watts')[0];
+        const results = StravaToCyclingMetricsConverter(time.data, power.data, hr.data);
+        assert.strictEqual(results.length, 3125, "Length of result should be 3125");
     }
 }
