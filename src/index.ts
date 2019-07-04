58594c9fff32a6d09da95c2cc0c7e88187a26561
Artur Tadrala <artur.tadrala@gmail.com>
2019-07-04 17:21:11 +0200

    Initial power duration curve

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,27 @@
-import { zip } from "lodash";
+import { zip, zipWith, map} from "lodash";
+import Heap from "heap-js";
+
+interface MetricsPoint {
+    time: Number;
+    power: Number;
+    heartRate: Number;
+}
+
+const getMaxPowerForInterval = (cycleMetrics: Array<MetricsPoint>, intervalLength ) => {
+    var i = 0;
+    var heap = new Heap<Number>();
+    var min = Number.MAX_SAFE_INTEGER;
+    for(i=0; i < intervalLength; i++){
+        heap.push(cycleMetrics[i].power);
+    }
+    min = Math.min(min, <number>heap.peek());
+    for(i=intervalLength; i < cycleMetrics.length; i++){
+        heap.remove(cycleMetrics[i-intervalLength].power);
+        heap.push(cycleMetrics[i].power);
+        min = Math.max(min, <number>heap.peek());
+    }
+    return min;
+}
 
 export const StravaToCyclingMetricsConverter = (secondsArr : Array<Number>, powerArr:Array<Number>, hrArray :Array<Number>) => {
     // prevent interpolating results array with undefined values
@@ -6,8 +29,18 @@ export const StravaToCyclingMetricsConverter = (secondsArr : Array<Number>, powe
         secondsArr.length !== hrArray.length ||
         powerArr.length !== hrArray.length)
         {
-            throw new Error("All arrays must be of equal length");
+            throw Error("All arrays must be of equal length");
         }
     
-    return zip(secondsArr, powerArr, hrArray);
+    return zipWith<Number,Number,Number, MetricsPoint>(secondsArr, powerArr, hrArray, (s,p,h) => {return {time:s, power: p, heartRate:h}});
+}
+
+export const PowerDurationCurve = ( cycleMetrics : Array<MetricsPoint>) => {
+    const length = cycleMetrics.length;
+    var result = new Array<number>(length);
+    for(var i =1; i < length; i++ )
+    {
+        result[i] = getMaxPowerForInterval(cycleMetrics, i);
+    }
+    return result;
 }
--- test/test.ts
+++ test/test.ts
@@ -1,14 +1,17 @@
 /// <reference types="jest" />
-import {StravaToCyclingMetricsConverter} from "../src/index";
+import {StravaToCyclingMetricsConverter, PowerDurationCurve} from "../src/index";
 import data from './sampleResponseStream.json';
 
 describe("Strava to Cycling Metrics converter", () => {
     test("smoke test", () => {
-        var time = [1,2,3,4];
-        var power = [110,120,130,140];
-        var hr = [90,92,93,94];
+        const time = [1,2,3,4];
+        const power = [110,120,130,140];
+        const hr = [90,92,93,94];
         expect(StravaToCyclingMetricsConverter(time, power, hr))
-        .toEqual( [[1,110,90], [2,120,92],[3,130,93], [4,140,94]]);
+        .toEqual( [{time:1,power:110,heartRate:90}, 
+                   {time:2,power:120,heartRate:92},
+                   {time:3,power:130,heartRate:93},
+                   {time:4,power:140,heartRate:94}]);
     });
 
     test("assert works on sample response stream", () => {
@@ -20,10 +23,74 @@ describe("Strava to Cycling Metrics converter", () => {
     });
     
     test("different lenths should throw", () => {
-        var time = [1,2];
-        var power = [110];
-        var hr = [90,92,93];
+        const time = [1,2];
+        const power = [110];
+        const hr = [90,92,93];
         expect(() => StravaToCyclingMetricsConverter(time, power, hr))
         .toThrow();
     })
+  });
+
+  describe("Power duration curve", () => {
+    test("should decrease linearly for linear power", () => {
+        const time = [0,1,2,3,4,5,6,7,8,9,10];
+        const power = [110,109,108,107,106,105,104,103,102,101,100];
+        const hr = new Array(11).fill(100);;
+        const metrics = StravaToCyclingMetricsConverter(time, power, hr);
+        expect(PowerDurationCurve(metrics))
+        .toEqual([undefined,110,109,108,107,106,105,104,103,102,101]);
+    });
+
+    test("should be const for const power", () => {
+        const time = [0,1,2,3,4,5,6,7,8,9,10];
+        const power = [110,110,110,110,110,110,110,110,110,110,110];
+        const hr = new Array(11).fill(100);;
+        const metrics = StravaToCyclingMetricsConverter(time, power, hr);
+        expect(PowerDurationCurve(metrics))
+        .toEqual([undefined,110,110,110,110,110,110,110,110,110,110]);
+    });
+
+    test("should descrease gradually for bell-like power spike", () => {
+        const time = [0,1,2,3,4,5,6,7,8,9,10];
+        const power = [110,110,112,112,114,116,114,112,112,110,110];
+        const hr = new Array(11).fill(100);;
+        const metrics = StravaToCyclingMetricsConverter(time, power, hr);
+        expect(PowerDurationCurve(metrics))
+        .toEqual([undefined,116,114,114,112,112,112,112,110,110,110]);
+    });
+
+    test("should interpolate undefined power value lineary", () => {
+        const time = [0,1,2,3,4,5,6,7,8,9,10];
+        const power = [undefined,110,112,112,undefined,116,undefined,112,112,110,undefined];
+        const hr = new Array(11).fill(100);;
+        const metrics = StravaToCyclingMetricsConverter(time, power, hr);
+        expect(PowerDurationCurve(metrics))
+        .toEqual([undefined,116,114,114,112,112,112,112,110,110,110]);
+    });
+
+    test("should interpolate missing Metrics points lineary", () => {
+        const time = [0,10];
+        const power = [110,100];
+        const hr = new Array(11).fill(100);;
+        const metrics = StravaToCyclingMetricsConverter(time, power, hr);
+        expect(PowerDurationCurve(metrics))
+        .toEqual([undefined,110,109,108,107,106,105,104,103,102,101]);
+    });
+
+    test("sample response stream", () => {
+        const time = <number[]> data.filter( x => x.type === 'time')[0].data;
+        const hr = <number[]> data.filter( x => x.type === 'heartrate')[0].data;
+        const power = <number[]> data.filter( x => x.type === 'watts')[0].data;
+        const metrics = StravaToCyclingMetricsConverter(time, power, hr).filter( m => m.power != undefined);
+        
+        const powerCurve = PowerDurationCurve(metrics);
+
+        const min = Math.min.apply(null, power);
+        const max = Math.max.apply(null, power);
+
+        powerCurve.forEach(power => {
+            expect(power).toBeLessThanOrEqual(max);
+            expect(power).toBeGreaterThanOrEqual(min);
+        });
+    });
   });
