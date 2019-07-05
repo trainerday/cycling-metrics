e20a947dc83ce79233112794fcd9a77e38d7b257
Alex VanLaningham <av@totxt.net>
2019-07-05 20:39:13 +0200

    adding ref

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
 import * as power from "./workout/power/index";
+
+
+export const StravaToCyclingMetricsConverter = power.StravaToCyclingMetricsConverter;
+export const PowerDurationCurve = power.PowerDurationCurve;
