7e1fab1bf57928d55bf9b0e5d56de5aaee58db15
Artur Tadrala <artur.tadrala@gmail.com>
2019-08-17 10:49:39 +0200

    refactor metrics

--- src/workout/metrics/index.ts
+++ src/workout/metrics/index.ts
@@ -1,51 +1,55 @@
-export const getIntensityFactor = (segments: number[][]) => {
-  if (segments.length === 0) {
-    return 0
-  }
-  if (segments[0].length !== 3) {
-    throw new Error ('must have 3 segments')
-  }
-  const inFac = getIntensityFactor(segments)
-  return inFac
+import _ from 'lodash';
+
+class WorkoutInterval {
+  Seconds: number;
+  StartPower: number;
+  EndPower: number;
 }
 
-export const getTss = (segments: number[][]) => {
-  if (segments.length === 0) {
-    return 0
+export class Workout {
+  private segments: Array<[number,number,WorkoutInterval]>;
+  private length: number;
+
+  constructor (segments: Array<WorkoutInterval>) {
+    if (segments.length === 0)
+      throw "Segments cannot be empty";
+    var time = 0;
+    this.segments = Array<[number,number,WorkoutInterval]>();
+    _.forEach(segments, s => {
+      this.segments.push ([time, time + s.Seconds, s]);
+      time = time + s.Seconds;
+    })
+    this.length = time; 
   }
-  if (segments[0].length !== 3) {
-    throw new Error ('must have 3 segments')
+  
+  private getValue(second:number) {
+    const [start, end, segment] = _.first(_.dropWhile(this.segments, s => s[1] <= second))
+    return segment.StartPower + (second - start) * (segment.EndPower - segment.StartPower) / segment.Seconds;
   }
 
-  const tss = getTss2(segments)
-  return tss
+  public Length = () => this.length; 
+
+  public static FromArray(segements : Array<[number,number,number]>) {
+    let workoutSegments = _.map(segements, ([Minutes,StartPower,EndPower]) => {return { Seconds: Minutes*60, StartPower, EndPower }})
+    return new Workout(workoutSegments);
+  }
+
+  public MovingAverage(intervalLength: number) {
+    var results = Array<number>();
+    var sum = _.sumBy(_.range(0,intervalLength), t => this.getValue(t));
+    results.push(sum / intervalLength);
+    for(var i = intervalLength+1; i < this.length; i++){
+      sum += this.getValue(i);
+      sum -= this.getValue(i-intervalLength-1);
+     results.push(sum / intervalLength);
+    };
+    return results;
+  }
 }
 
-
-
-
-
-
-function getTss2(arr){
-  const FTP = 100// we are not using power we are using percent of FTP.
-  const ifObj = getIntensityFactor2(arr)
-  const IF = ifObj.if
-  const NP = ifObj.np
-  const t = ifObj.seconds
-  const TSS = Math.round(((t * NP * IF) / (FTP * 3600)) * 100) 
-  return TSS
-}
-
-
-
-function getIntensityFactorInt(arr){
-  const if2= getIntensityFactor2(arr).if
-  return Math.floor(if2 * 100)/100
-}
-
-function getIntensityFactor2(arr){
+export function getIntensityFactor2(workout: Workout){
   const FTP=100// we are not using power we are using percent of FTP.
-  const np = getNormalizedPower(arr)
+  const np = getNormalizedPower(workout)
   const NP = np.np
   const IF = (NP/FTP)
   const out = {if:IF,seconds:np.seconds,np:NP}
@@ -53,68 +57,29 @@ function getIntensityFactor2(arr){
   return out
 }
 
+export function getTss(workout:Workout){
+  const FTP = 100// we are not using power we are using percent of FTP.
+  debugger;
+  const ifObj = getIntensityFactor2(workout)
+  console.log(ifObj)
+  const IF = ifObj.if
+  const NP = ifObj.np
+  const t = ifObj.seconds
+  const TSS = Math.round(((t * NP * IF) / (FTP * 3600)) * 100) 
+  return TSS
+}
 
-function getNormalizedPower(arr){
-  const res = getOneSecondIntervals(arr)
-  if (res.length<120) {return {np:0,seconds:0}}
+function getIntensityFactorInt(workout: Workout){
+  const if2= getIntensityFactor2(workout).if
+  return Math.floor(if2 * 100)/100
+}
 
-  const mov = movingAvg(res,30)
-  const filtered = mov.filter(el => {
-    return el != null
-  })
-
-  const pow4 = []
-  filtered.forEach(item=>{
-    pow4.push(Math.pow(item,4))
-  })
-  const avgPow4 = average(pow4)
+function getNormalizedPower(workout: Workout){
+  if (workout.Length() < 120) {
+    return {np:0, seconds:0}
+  }
+  const mvAvg = workout.MovingAverage(30)
+  const avgPow4 = _.sumBy(mvAvg, x => Math.round(Math.pow(x,4)*100)/100) / mvAvg.length
   const avg = Math.round(Math.pow(avgPow4,.25)*100)/100
-  return {np:avg,seconds:res.length} 
+  return {np:avg, seconds:workout.Length()} 
 }
-
-
-function average(list){
-  const averageLoc = list2 => list2.reduce((prev, curr) => prev + curr) / list2.length;
-  return Math.round(averageLoc(list)*100)/100;
-}
-
-function getOneSecondIntervals(arr){
-  const out = [];
-  arr.forEach(segment=>{
-  const min=segment[0]
-  const pStart=segment[1]
-  const pEnd=segment[2]
-  const seconds=min*60
-  const adder=(pEnd-pStart)/seconds
-  let first=pStart
-  for (let i = 0; i<seconds; i++) {
-    out.push(Math.round(first*100)/100)
-      first +=adder
-    }
-  })
-  return out
-}
-
-function movingAvg(array, count){
-  const avg = (array2) =>{
-    let sum = 0
-    array2.forEach(item=>{
-      sum += item
-    })
-    return sum / array2.length
-  }
-
-  const result = []
-  let val
-
-  for (let i = 0; i < count - 1; i++) {result.push(null)}
-  const len = array.length - count
-  for (let i = 0; i <= len; i++){
-    val = avg(array.slice(i, i + count))
-    if (isNaN(val)) {result.push(null)}
-    else {result.push(Math.round(val*100)/100)}
-  }
-
-  return result;
-}
-
