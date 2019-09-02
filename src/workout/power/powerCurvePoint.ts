8a5a3af875cf5ebeacfe06786ca94dfe02df501a
Alex <av@totxt.net>
2019-09-02 16:43:26 +0200

    adding back file

---
+++ src/workout/power/powerCurvePoint.ts
@@ -0,0 +1,15 @@
+export const WithLabel = (point: PowerCurvePoint, label: string) => {
+  return label === undefined
+    ? point
+    : {
+        label,
+        power: point.power,
+        time: point.time,
+      };
+};
+
+export class PowerCurvePoint {
+  public time: number;
+  public label: string;
+  public power: number;
+}
