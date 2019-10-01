21076de66c1a5c74614cf0b19e41106870f5168b
avanlaningham <av@totxt.net>
2019-10-01 10:30:59 +0200

    made CTL very FP style

--- src/workouts/volume/clt.test.ts
+++ src/workouts/volume/clt.test.ts
@@ -1,61 +1,71 @@
 import * as _ from 'lodash/fp'
-import { getCtl } from './clt'
+import { getCtl, getCtlSingle } from './clt'
 import moment from 'moment'
 
-
-
-test('test', () => {
-  const getDate = (dayNum: number) => moment('2019-09-30T00:00:00.000Z').add(dayNum, 'days').toDate()
-  const getObj = (dayNum: number, tss: number) => ({date: getDate(dayNum), tss: tss})
-  const twoWeekArr = _.map((ele: number)=>getObj(ele,70),[0,2,4,7,9,11])
-//  console.log(twoWeekArr)
-  expect(twoWeekArr![0].tss).toBe(70)
+describe('getCtlSingle - 100', () => {
+  test('getCtlSingle', () => {
+    const res = getCtlSingle(100,0)
+    expect(res).toBe(2.4)
+  })
+  test('getCtlSingle - 100-d2', () => {
+    const res = getCtlSingle(100,2.4)
+    expect(res).toBe(4.7)
+  })
+  test('getCtlSingle - 70', () => {
+    const res = getCtlSingle(70,0)
+    expect(res).toBe(1.7)
+  })
 })
 
-test('test2', () => {
-  const day = [100,0,100,0,100,0,0,100,0,100,0,100,0,0]
-  const res = getCtl(day,0, 80)
-  const max:number = _.max(res)!
-  expect(max).toBe(12.4)
-  expect(res[20]).toBe(9.9)
-})
 
-test('test2', () => {
-  const day = [70,0,70,0,70,0,0,130,0,130,0,130,0,0]
-  const res = getCtl(day,0, 80)
-  const max:number = _.max(res)!
-  expect(max).toBe(13)
-  expect(res[20]).toBe(10.3)
-})
+describe('Ctl', () => {
+  test('test', () => {
+    const getDate = (dayNum: number) => moment('2019-09-30T00:00:00.000Z').add(dayNum, 'days').toDate()
+    const getObj = (dayNum: number, tss: number) => ({date: getDate(dayNum), tss: tss})
+    const twoWeekArr = _.map((ele: number)=>getObj(ele,70),[0,2,4,7,9,11])
+  //  console.log(twoWeekArr)
+    expect(twoWeekArr![0].tss).toBe(70)
+  })
 
+  test('test2', () => {
+    const day = [100,0,100,0,100,0,0,100,0,100,0,100,0,0]
+    const res = getCtl(day,0, 80)
+    const max:number = _.max(res)!
+    expect(max).toBe(12.4)
+    expect(res[20]).toBe(9.9)
+  })
+
+  test('test2', () => {
+    const day = [70,0,70,0,70,0,0,130,0,130,0,130,0,0]
+    const res = getCtl(day,0, 80)
+    const max:number = _.max(res)!
+    expect(max).toBe(13)
+    expect(res[20]).toBe(10.3)
+  })
+})
 
 
 describe('TSS', () => {
-  test('one day of 70tss should be 1.67 tss', () => {
-    const days = 30
-    const startCtl = 0
-    const dailyTss = getDailyTss()
-    const res = getCtl(dailyTss, startCtl, days)
-    expect(res[0]).toBe(1.7)
-  })
-
   test('21 days of 70tss should be 27.8 tss', () => {
     const days = 30
     const startCtl = 0
-    const dailyTss = getDailyTss()
+    const dailyTss = getDailyTssForTests()
     const res = getCtl(dailyTss, startCtl, days)
     expect(res[20]).toBe(27.8)
+    expect(res.length).toBe(days)
+
   })
 
   test('21 days of 70tss should be 32 tss on day 31', () => {
     const days = 30
     const startCtl = 0
-    const dailyTss = getDailyTss()
+    const dailyTss = getDailyTssForTests()
     const res = getCtl(dailyTss, startCtl, days)
-    expect(res[30]).toBe(32)
+    console.log(res.length)
+    expect(res[29]).toBe(32.8)
   })
 
-  function getDailyTss(): number[] {
+  function getDailyTssForTests(): number[] {
     const dailyTss = []
     for (let i = 0; i <= 27; i++) {
       dailyTss.push(70)
--- src/workouts/volume/clt.ts
+++ src/workouts/volume/clt.ts
@@ -1,39 +1,18 @@
-import { WorkoutStats } from '../../models/workoutStats'
 import _ from 'lodash'
 
+export const getCtlSingle = (tss: number, yesterdayCtl:number) => Math.round(((tss - yesterdayCtl) / 42 + yesterdayCtl)*10)/10
 
-export const getCtl = (dailyTss: number[], ctl: number, days: number, ) :number [] => {
-  const getTssIfExistsOrZero = (dailyTss: number) => dailyTss?dailyTss:0
-  const getCtl = (tss: number, yesterdayCtl:number) => Math.round(((tss - yesterdayCtl) / 42 + yesterdayCtl)*10)/10
-  const dayArr = _.range(days+1)
-  const dayArr1 = _.map(dayArr, (day: number,index: number) => ({day: day, tss:dailyTss[index]}))
-  dayArr1.forEach((day: any) => {
-    ctl = getCtl(getTssIfExistsOrZero(day.tss), ctl)
-    day.ctl = ctl
-  })
-  return _.map(dayArr1, 'ctl')
+//todo add tests make sure elementsToAdd > 0, or shorten array?
+const addZeroElementsToNumberArray = (arr: number[], elementsToAdd: number) => [...arr,..._.times(elementsToAdd, _.constant(0))]
+
+//todo add tests
+export const getCtlDays = (days: any, ctlIn: number) => {
+  let ctl = ctlIn
+  return _.map(days, tss => {ctl = getCtlSingle(tss,ctl); return ctl})
 }
 
-export const getCtlOld = (dailyTss: number[], days: number, startCtl: number) => {
-  const out = []
-
-  for (let i = 0; i <= days; i++) {
-    let tss = 0
-    if (i < dailyTss.length) {
-      tss = dailyTss[i]
-    }
-    let ctl = startCtl
-    let yesterdayCtl = 0
-    if (i > 0) {
-      yesterdayCtl = out[i - 1].ctl
-    }
-    ctl = (tss - yesterdayCtl) / 42 + yesterdayCtl
-    const obj = new WorkoutStats()
-    obj.day = i
-    obj.ctl = Math.round(ctl * 10) / 10
-    obj.tss = tss
-    out.push(obj)
-  }
-
-  return out
+// this is main method for export
+export const getCtl = (dailyTss: number[], ctlStart: number, dayCount: number, ) :number [] => {
+  const allDays = addZeroElementsToNumberArray(dailyTss, dayCount - dailyTss.length)
+  return getCtlDays(allDays, ctlStart)
 }
