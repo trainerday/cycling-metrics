e334b115dad90a9530d4fd6d19658b18f82e302c
AndreyKomin <andrey.s.komin@gmail.com>
2021-08-23 14:09:46 +0300

    fix: lint files

--- src/common/insertMissingElements.test.ts
+++ src/common/insertMissingElements.test.ts
@@ -3,6 +3,6 @@ import { insertMissingElementsInSequence } from './insertMissingElements'
 describe('moving average test', (): void => {
   test('Moving average on the ramp', (): void => {
     const res = insertMissingElementsInSequence([1, 2, 5, 8], [2, 2, 10, 10])
-    expect(res).toEqual([2,2,0,0,10,0,0,10])
+    expect(res).toEqual([2, 2, 0, 0, 10, 0, 0, 10])
   })
 })
--- src/index.test.ts
+++ src/index.test.ts
@@ -1,22 +1,21 @@
-import { getMeanMaxPowerCurve, getTrainingStress,getMergedCurve } from './index'
+import { getMeanMaxPowerCurve, getTrainingStress, getMergedCurve } from './index'
 import _ from 'lodash'
 
 describe('Index', () => {
   test('power duration curve', () => {
     const power = [102, 106, 110, 114, 118, 120, 116, 112, 108, 104, 100]
     const curve = getMeanMaxPowerCurve(power)
-    expect(curve).toEqual([120,119,118,117,116,115,114,113,112,111,110])
+    expect(curve).toEqual([120, 119, 118, 117, 116, 115, 114, 113, 112, 111, 110])
   })
   test('training stress', () => {
-    const intervals: [number, number, number] [] = [[60,200,200]] // minutes, wattsStart, wattsEnd
+    const intervals: [number, number, number][] = [[60, 200, 200]] // minutes, wattsStart, wattsEnd
     const trainingStress = getTrainingStress(intervals, 200)
     expect(trainingStress).toEqual(100)
   })
   test('mergedCurve', () => {
-    const pow1 = _.range(1,10000)
-    const pow2 = _.range(1,10000)
+    const pow1 = _.range(1, 10000)
+    const pow2 = _.range(1, 10000)
     const curve = getMergedCurve(pow1, pow2)
-    expect(curve.length).toEqual(10000-1)
+    expect(curve.length).toEqual(10000 - 1)
   })
 })
-
